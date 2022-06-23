761c459144c119ca5e2b0f53cb1e7ce379f833d5
okellogerald <okellogeralddev@gmail.com>
2022-06-23 11:06:20 +0300

    minor update

--- lib/blocs/report/models/annotation.dart
+++ lib/blocs/report/models/annotation.dart
@@ -2,7 +2,7 @@ class Annotation {
   final String title, shortTitle, type;
   const Annotation(this.title, this.shortTitle, this.type);
 
-  const Annotation.empty() : this('', '', '');
+  const Annotation.empty() : this('item', 'amount', '');
 
   factory Annotation.fromMap(Map<String, dynamic> rawMap) {
     return Annotation(rawMap['title'], rawMap['shortTitle'], rawMap['type']);
